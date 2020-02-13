27a7b40dd9a571a4cdb11d7a760849485c859aa9
feridun <59169551+feridunAKYOL@users.noreply.github.com>
2020-02-13 23:29:03 +0100

    Update handler.js

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,7 +1,5 @@
-function calculator(event) {
-
+function calculateHandler(event) {
   // read & process user input from event
-
   var a= parseInt(document.querySelector('#firstNumber').value);
   var b= parseInt(document.querySelector('#secondNumber').value);
   var op = document.querySelector('#operator').value;
@@ -9,12 +7,8 @@ function calculator(event) {
  const calculated= doMath(op, a, b);
   // render output to DOM for user
   document.querySelector('#result').innerHTML = calculated;
-
- 
   // log user action for developers
-    console.log(calculate);
-  
+
   // return true for the browser
   return true;
 }
-
