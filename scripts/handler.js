c7501d05655952d09fbad9813bc6ceafb61303ee
feridun Akyol <feridunakyol34@gmail.com>
2020-02-13 23:16:17 +0100

    last changes

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,10 +1,12 @@
 function calculateHandler(event) {
   // read & process user input from event
-
+  var a= parseInt(document.querySelector('#firstNumber').value);
+  var b= parseInt(document.querySelector('#secondNumber').value);
+  var op = document.querySelector('#operator').value;
   // pass user input through core logic
-
+ const calculated= doMath(op, a, b);
   // render output to DOM for user
-
+  document.querySelector('#result').innerHTML = calculated;
   // log user action for developers
 
   // return true for the browser
--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,7 @@
+function calculator (op,a,b){
+  
+
+}
 // test cases for your doMath function
 // these are all correct, you don't need to change them
 const doMathTests = [
@@ -25,10 +29,10 @@ const doMathTests = [
 ];
 
 // refactor the logic from the calculator tutorial into this function
-function doMath(operation, a, b) {
+function doMath(op, a, b) {
   // these if statements make sure all arguments are the correct type
   // they will throw errors if your handler passes the wrong type arguments
-  if (typeof operation !== 'string') {
+  if (typeof op !== 'string') {
     throw new Error('operation should be a string');
   }
   if (typeof a !== 'number') {
@@ -38,25 +42,17 @@ function doMath(operation, a, b) {
     throw new Error('b should be a number');
   }
   // write your code below this comment:
-
-  function calculator() {
-
-    var a= parseInt(document.querySelector('#firstNumber').value);
-    var b= parseInt(document.querySelector('#secondNumber').value);
-    var op = document.querySelector('#operator').value;
-
-    var calculate;
-  
-    if (op == 'add'){
-      calculate = a + b;
-    } else if (op == 'min'){
-      calculate = a - b;
-    } else if (op == 'div'){
-      calculate = a / b;
-    } else if (op == 'mult'){
-      calculate = a * b;
-    }
-
-    document.querySelector('#result').innerHTML = calculate;
+  if (op == 'add'){
+    calculate = a + b;
+} else if (op == 'min'){
+    calculate = a - b;
+} else if (op == 'div'){
+    calculate = a / b;
+} else if (op == 'mult'){
+    calculate = a * b;
+}
+return calculate;
+    
   }
+
 testing(doMath, doMathTests);
